80b3c0dd1e1e78a2b176dc46e64d8c3bab643d73
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2018-05-18 19:54:09 -0600

    added icons

---
+++ main.js
@@ -0,0 +1,8 @@
+(function () {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//cdn.youracclaim.com/assets/utilities/embed.js';
+  var o = document.getElementsByTagName('script')[0];
+  o.parentNode.insertBefore(s, o);
+})();

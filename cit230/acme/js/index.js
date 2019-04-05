def61e330dd2d93da4005629eec1cf41d8d2bba8
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2019-04-04 21:13:22 -0600

    added dynamic page builds

--- cit230/acme/js/index.js
+++ cit230/acme/js/index.js
@@ -1,5 +1,9 @@
+const pageContent = document.getElementById("page-content");
+const original = pageContent.innerHTML;
+
+// Fetch the data from the API
 let fetchData = () => {
-  fetch("../js/acme.json")
+  fetch("./js/acme.json")
     .then(res => {
       if (res.ok) {
         return res.json();
@@ -16,30 +20,49 @@ let fetchData = () => {
       // Build the navbar
       buildNav(data, keys);
     });
-  // .then(data => {
-  //   // buildPage(data);
-  // });
 };
 
+// Build the navigation
 let buildNav = (data, keys) => {
   const navList = document.getElementById("nav-list");
 
+  //  Create a nav list item for every key
   for (const key of keys) {
-    navList.innerHTML += `<li><a id=${key.toLowerCase()}>${key}</a></li>`;
-    document.getElementById(key.toLowerCase()).addEventListener("click", () => {
-      // buildPage(data, key);
-      console.log("clicked!");
-    });
+    navList.innerHTML += `<li><a href="#" id=${key.toLowerCase()}>${key}</a></li>`;
   }
 
-  console.log(navList);
+  // Create an event listener for the home page
+  document.getElementById("home").addEventListener("click", () => {
+    buildHome(original);
+  });
+
+  // Create an event listener for every nav list item
+  keys.forEach(key => {
+    document.getElementById(key.toLowerCase()).addEventListener("click", () => {
+      // If clicked, build the page with the object info
+      buildPage(data[key]);
+    });
+  });
 };
 
-let buildPage = (data, key) => {
-  // const pageContent = document.getElementById("page-content");
-  // pageContent.innerHTML = `
-  //   <h2>${data.name.filter(key)}</h2>
-  // `;
+// Build the home page
+let buildHome = () => {
+  pageContent.innerHTML = original;
+};
+
+// Build the nav pages
+let buildPage = data => {
+  console.log(data);
+
+  // Populate the HTML with the object info
+  pageContent.innerHTML = `
+    <h2>${data.name}</h2>
+    <img src="${data.path}" alt="${data.name}"></img>
+    <p>${data.description}</p>
+    <p><strong>Made by:</strong> ${data.manufacturer}</p>
+    <p><strong>Reviews:</strong> ${data.reviews}/5 stars</p>
+    <h3>$${data.price}</h3>
+  `;
 };
 
 fetchData();

39888edd0c0330696032533accc6df5ee51fbdcf
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2019-04-11 17:45:00 -0600

    added styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,8 @@ module.exports = {
       },
     },
     `gatsby-plugin-sass`, // Added for SASS
+    `gatsby-plugin-styled-components`, // Added for Styled Components
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,36 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
+const Inner = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const Heading = styled.h1`
+  margin: 0;
+`
+
+const HeadingLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <Container>
+    <Inner>
+      <Heading>
+        <HeadingLink to="/">{siteTitle}</HeadingLink>
+      </Heading>
+    </Inner>
+  </Container>
 )
 
 Header.propTypes = {

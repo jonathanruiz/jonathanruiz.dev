8d223722760d108630b4250c80507705e8b30b99
Johnny Ruiz <johnny@ruizops.com>
2023-07-06 17:31:06 -0600

    removed next-theme

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,19 +1,21 @@
 "use client";
 
 import * as React from "react";
-import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/icons";
 
 export function ThemeToggle() {
-  const { setTheme, theme } = useTheme();
+  const [theme, setTheme] = React.useState<"light" | "dark">("light");
 
   return (
     <Button
       variant="ghost"
       size="sm"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      onClick={() => {
+        setTheme(theme === "light" ? "dark" : "light");
+        console.log(theme);
+      }}
     >
       <Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />

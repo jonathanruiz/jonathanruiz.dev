10b8cebb9e0da4269e1f62d6257ff7d26fff2140
Johnny Ruiz <johnny@ruizops.com>
2023-07-06 20:31:35 -0600

    added ThemeToggle component

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,25 +1,31 @@
-"use client";
-
-import * as React from "react";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/icons";
 
 export function ThemeToggle() {
-  const [theme, setTheme] = React.useState<"light" | "dark">("light");
+	const [theme, setTheme] = useState(localStorage.theme);
+	const colorTheme = theme === "dark" ? "light" : "dark";
 
-  return (
-    <Button
-      variant="ghost"
-      size="sm"
-      onClick={() => {
-        setTheme(theme === "light" ? "dark" : "light");
-        console.log(theme);
-      }}
-    >
-      <Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
-    </Button>
-  );
+	useEffect(() => {
+		const root = window.document.documentElement;
+		root.classList.remove(colorTheme);
+		root.classList.add(theme);
+		localStorage.setItem("theme", theme);
+	}, [theme, colorTheme]);
+
+	return (
+		<Button
+			variant="ghost"
+			size="sm"
+			onClick={() => {
+				setTheme(theme === "light" ? "dark" : "light");
+				console.log(theme);
+			}}
+		>
+			<Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+			<Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+			<span className="sr-only">Toggle theme</span>
+		</Button>
+	);
 }

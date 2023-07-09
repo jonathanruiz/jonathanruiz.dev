426a666b526332191416ca4dd79ff14a583b7591
Johnny Ruiz <johnny@ruizops.com>
2023-07-08 21:53:55 -0600

    Added navbar for new portfolio (#21)
    * added theme components
    
    * simplified nav
    
    * simplified nav even further
    
    * removed next-theme
    
    * fixed index page
    
    * added favicon link
    
    * added ThemeToggle component
    
    * made theme toggle via astr
    
    * fixed toggle button
    
    * fixed typo
    
    * renamed MainLayout
    
    * changed body
    
    * changed globals.css
    
    * fixed tailwind not loading
    
    * created navbar template
    
    * added Nav component
    
    * fixed transition
    
    * prettier config updated
    
    * added github link
    
    * added slot
    
    * added sample data
    
    * added main nav routes
    
    * update links for github

---
+++ config/site.ts
@@ -0,0 +1,26 @@
+
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+    name: "Jonathan Ruiz",
+    description:
+        "A portfolio site for Jonathan Ruiz, a software engineer based in Utah",
+    mainNav: [
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Projects",
+            href: "/projects",
+        },
+        {
+            title: "Blog",
+            href: "/blog",
+        }
+    ],
+    links: {
+        github: "https://github.com/jonathanruiz",
+        linkedin: "https://www.linkedin.com/in/jonathan-ruiz-9a3038168/",
+    },
+}

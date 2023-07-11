ca8fdbddb31cee39c08b414de0b1dcb0ae32e5e1
Johnny Ruiz <johnny@ruizops.com>
2023-07-10 21:18:05 -0600

    create cards for each project

--- config/site.ts
+++ config/site.ts
@@ -25,18 +25,25 @@ export const siteConfig = {
     projects: [
         {
             title: "Portfolio",
+            description: "This site you're on right now!",
             link: "https://jonathanruiz.dev",
-            githubRepo: "https://github.com/jonathanruiz/jonathanruiz.dev"
+            githubRepo: "https://github.com/jonathanruiz/jonathanruiz.dev",
+            tags: ["Astro", "TailwindCSS"]
         },
         {
             title: "Hacker News Next-js",
+            description: "A Hacker News clone built with Next.js.",
             link: "",
-            githubRepo: "https://github.com/jonathanruiz/hacker-news-nextjs"
+            githubRepo: "https://github.com/jonathanruiz/hacker-news-nextjs",
+            tags: ["Next.js", "TypeScript", "TailwindCSS"]
         },
         {
             title: "nato-reader",
+            description:
+                "A CLI tool to read an input as the NATO phonetic alphabet.",
             link: "https://github.com/jonathanruiz/nato-reader",
-            githubRepo: "https://github.com/jonathanruiz/hacker-news-nextjs"
+            githubRepo: "https://github.com/jonathanruiz/hacker-news-nextjs",
+            tags: ["GoLang", "CLI"]
         }
     ]
 }
